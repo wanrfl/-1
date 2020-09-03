1427d1c7b8f648d941222d489f3b008a86ee6a4c
任飞龙 <3021916452@qq.com>
2020-09-03 09:40:54 +0800

    后台管理系统

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports={
+    devServe:{
+        port:8001,
+        open:true,
+        https:false,
+        host:"localhost",
+    },
+    lintOnSave:false,
+    outputDir:"dist2",
+    assetsDir:"assets",
+    indexPath:"out/index.html",
+    productionSourceMap:false,
+};
